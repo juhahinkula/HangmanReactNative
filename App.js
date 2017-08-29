14d7ca3395ee05a35f823a0987d4c86661952b88
juhahinkula <hinkula@gmail.com>
2017-08-29 09:09:54 +0300

    component structure changed

--- App.js
+++ App.js
@@ -1,34 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import Keyboard from '../components/Keyboard';
 
 export default class App extends React.Component {
-  state = {word: ''};
-
-  _handlePress(buttonTitle) {
-    console.log(buttonTitle);
-    this.setState({word: this.state.word + buttonTitle});
-  }
-  
-  letters1 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
-  letters2 = ['J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'];
-  letters3 = ['S', 'T', 'U', 'V', 'X', 'Y', 'Z'];
   
   render() {
     return (
       <View style={styles.container} >
-        <Text>{this.state.word}</Text>
-        <View style={styles.buttonContainer}  >
-         { this.letters1.map((_, index) => 
-            <Button key={this.letters1.index} title={this.letters1[index]} onPress={() => this._handlePress(this.letters1[index])} />) }
-        </View> 
-        <View style={styles.buttonContainer}  >
-         { this.letters2.map((_, index) => 
-            <Button key={this.letters2.index} title={this.letters2[index]} onPress={() => this._handlePress(this.letters2[index])} />) }
-        </View>
-        <View style={styles.buttonContainer}  >
-          { this.letters3.map((_, index) => 
-            <Button key={this.letters3.index} title={this.letters3[index]} onPress={() => this._handlePress(this.letters3[index])} />) }  
-        </View>
+        <Keyboard/>
       </View>
     );
   }
@@ -40,15 +19,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },  
-  letterButton: {
-    marginRight: 5
   }
 });
