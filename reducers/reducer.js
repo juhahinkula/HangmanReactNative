683c23a16a97cc5b85d3b4307125e77f57432fbe
juhahinkula <hinkula@gmail.com>
2017-08-30 12:00:06 +0300

    adding redux

--- reducers/reducer.js
+++ reducers/reducer.js
@@ -4,10 +4,7 @@ const initialState = {
   letter: ''
 }
 
-// This is a reducer which listens to actions and modifies the state
 const reducer = (state = initialState, action) => {
-  // A switch is used since if more actions are added in the future, it will be easy
-  // to be able to handle this in the reducer since we just add another 'case'.
   switch (action.type) {
     case LETTER_KEY_PRESSED:
       return {
